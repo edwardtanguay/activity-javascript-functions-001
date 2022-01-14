af704bae76a619115cfbda44ae5fcf9ef14dd4d1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-14 12:56:04 +0100

    lodash

--- ex003.test.js
+++ ex003.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('@jest/globals');
-const { isPlainObject, makePairs } = require('./ex003');
+const { isPlainObject, makePairs, without, isEmpty, isEqualDeep } = require('./ex003');
 
 test('isPlainObject() is correctly implemented', () => {
 	expect(isPlainObject('aaaaaaaaa')).toBe(false);
@@ -13,5 +13,28 @@ test('isPlainObject() is correctly implemented', () => {
 });
 
 test('makePairs() is correctly implemented', () => {
-	expect(makePairs({ a: 1, b: 2 })).toBe([['a', 1], ['b', 2]]);
+	expect(makePairs({ a: 1, b: 2 })).toStrictEqual([['a', 1], ['b', 2]]);
+	expect(makePairs({ a: 1, ok: 2, name: 'Georg' })).toStrictEqual([['a', 1], ['ok', 2], ['name', 'Georg']]);
+	expect(makePairs({})).toStrictEqual([]);
+	expect(makePairs(null)).toStrictEqual([]);
+	expect(makePairs([])).toStrictEqual([]);
+	expect(makePairs([1,2,3])).toStrictEqual([]);
+	expect(makePairs(false)).toStrictEqual([]);
+	expect(makePairs(true)).toStrictEqual([]);
+});
+
+test('without() is correctly implemented', () => {
+	expect(without({ a: 1, b: 2 }, 'b')).toStrictEqual({ a: 1 });
+	expect(without({ a: 1, b: 2 }, 'a')).toStrictEqual({ b: 2 });
+	expect(without({ a: 1, b: 2 }, 'c')).toStrictEqual({ a: 1, b: 2 });
+});
+
+test('isEmpty() is correctly implemented', () => {
+	expect(isEmpty({ a: 1, b: undefined })).toBe(false);
+	expect(isEmpty({})).toBe(true);
+});
+
+test('isEqualDeep() is correctly implemented', () => {
+	expect(isEqualDeep({ a: 1, b: { c: 1 } },{ a: 1, b: { c: 1 } })).toBe(true);
+	expect(isEqualDeep({ a: 1, b: { c: 1 } },{ a: 1, b: { c: 2 } })).toBe(false);
 });
